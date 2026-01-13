8b19f5e5013a24a02e237b836e7fcb6a0ee3c93f
Yoonsang Lee <ysle@pm.me>
2026-01-13 23:59:29 +0900

    fix: OverlappedExt OVERLAPPED 상속 제거
    - OVERLAPPED 상속으로 인한 메모리 레이아웃 문제 해결
    - WSAOVERLAPPED가 첫 번째 멤버로 위치하여 캐스팅 정상 동작
    - reinterpret_cast<OverlappedExt*>(lpOverlapped) 안전하게 사용 가능

--- network/OverlappedExt.h
+++ network/OverlappedExt.h
@@ -4,13 +4,15 @@
 #include "Const.h"
 
 namespace highp::network {
-struct OverlappedExt : OVERLAPPED {
-	// WSAOVERLAPPED must be first for correct casting to LPWSAOVERLAPPED
-	WSAOVERLAPPED overlapped;
+struct OverlappedExt {
+	// WSAOVERLAPPED must be FIRST member for correct casting
+	// reinterpret_cast<OverlappedExt*>(lpOverlapped) 가 올바르게 동작하려면
+	// overlapped 멤버가 구조체 첫 번째에 위치해야 함
+	WSAOVERLAPPED overlapped{};
 	EIoType ioType{ EIoType::Accept };
 	SocketHandle clientSocket = InvalidSocket;
 	WSABUF wsaBuffer{};
-	char buffer[Const::socketBufferSize];
+	char buffer[Const::socketBufferSize]{};
 
 	// Overlapped struct 는 등록되고나서 주소 변경이나 이동이 일어나면
 	// mem corruption 이 발생할 수 있으므로 복사 및 이동 금지
