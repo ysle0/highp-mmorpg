23c364d8b801003d334e7b4270fb13859554ed94
Yoonsang Lee <ysle@pm.me>
2026-02-27 01:25:49 +0900

    feat(objpool): object pool 의 자원해제 위험을 RAII 로 해결

--- lib/inc/memory/HybridObjectPool.hpp
+++ lib/inc/memory/HybridObjectPool.hpp
@@ -1,7 +1,10 @@
 ﻿#pragma once
+
 #include <concepts>
 #include <mutex>
 #include <vector>
+
+#include "scope/DeferContext.hpp"
 
 namespace highp::mem {
     struct HybridObjectPoolConfig {
@@ -47,26 +50,27 @@ namespace highp::mem {
         // static only
         HybridObjectPool() = delete;
 
-        static T* Get() {
+        static scope::DeferContext<T> Get() {
             LocalStorage& local = GetLocalStorage();
 
             // thread local 에 있으면 바로 꺼내줌 (lock-free)
             if (!local.pool.empty()) {
                 T* item = local.pool.back();
                 local.pool.pop_back();
-                return item;
+
+                return scope::DeferContext<T>{
+                    item,
+                    [](T* x) { HybridObjectPool::Return(x); }
+                };
             }
 
             // 없으면 chunk 하나 때옴
             {
                 std::lock_guard lock(_globalMtx);
-                const int size = static_cast<int>(_globalPool.size());
 
                 // global 에 여유가 있으면 가져옴
-                if (size > 0) {
-                    const int fetchCount = size > HybridObjectPoolConfig::ChunkSize
-                                               ? HybridObjectPoolConfig::ChunkSize
-                                               : size;
+                if (const int size = static_cast<int>(_globalPool.size()); size > 0) {
+                    const int fetchCount = min(size, HybridObjectPoolConfig::ChunkSize);
                     auto from = _globalPool.end() - fetchCount;
                     local.pool.insert(local.pool.end(), from, _globalPool.end());
                     _globalPool.erase(from, _globalPool.end());
@@ -74,14 +78,21 @@ namespace highp::mem {
             }
 
             // global 에서 꺼내도 여전히 없으면 새로 만들어서 바로 반환
-            // 어짜피 이후 Return() 으로 Pool 에 다시 돌아옴.
+            // 어짜피 이후 RAII 로 Pool 에 다시 돌아옴.
             if (local.pool.empty()) {
-                return new T();
+                return scope::DeferContext<T>{
+                    new T(),
+                    [](T* x) { HybridObjectPool::Return(x); }
+                };
             }
 
             T* item = local.pool.back();
             local.pool.pop_back();
-            return item;
+
+            return scope::DeferContext<T>{
+                item,
+                [](T* x) { HybridObjectPool::Return(x); }
+            };
         }
 
         static void Return(T* item) {
@@ -102,20 +113,11 @@ namespace highp::mem {
             }
         }
 
-        /// <summary>
         /// global pool 을 안전하게 해제하려면 여기서 chunk 를 때간
-        /// thread local pool 들을 모두 join 시켜 소멸자에서 전부 global pool 로 빌려간 object들을 돌려줘야함
-        /// -> 규약으로 모든 스레드들을 join ->  ClearGlobal() 강제?
+        /// thread local.Pool.들을 모두 join 시켜 소멸자에서 전부 global pool 로 빌려간 object들을 돌려줘야함
+        /// -> 규약으로 모든 스레드들을 join..
         /// 
         /// 애초에 프로세스 종료됨에 따라 OS가 메모리 회수해갈거고, 프로세스가 살아있으면서
         /// pool 을 초기화 하는 시나리오..? 게임서버에서 그런 경우가 있을지 모르겠음.
-        /// </summary>
-        //static void ClearGlobal() {
-        //	std::lock_guard lock(_globalMtx);
-        //	for (T* item : _globalPool) {
-        //		delete item;
-        //	}
-        //	_globalPool.clear();
-        //}
     };
 }
