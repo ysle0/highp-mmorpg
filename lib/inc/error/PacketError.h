b1763f837febff61ebbc7300d0943ac002ddb2f4
Yoonsang Lee <ysle@pm.me>
2026-03-01 02:45:03 +0900

    chore(err): add packet error

---
+++ lib/inc/error/PacketError.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+namespace highp::err {
+    enum class EPacketError {
+        Unknown = 0,
+
+        FbFailedVerify,
+        FbFailedSerialize,
+        FbGetPacket,
+        FbFailedDeserialize,
+        FbPayloadTypeNotFound,
+    };
+
+    constexpr std::string_view toString(EPacketError err) noexcept {
+        switch (err) {
+        case EPacketError::FbFailedVerify: return "PacketFlatbufferFailedVerify";
+        case EPacketError::FbFailedSerialize: return "PacketFlatbufferFailedSerialize";
+        case EPacketError::FbGetPacket: return "PacketFlatbufferGetPacket";
+        case EPacketError::FbFailedDeserialize: return "PacketFlatbufferFailedDeserialize";
+        case EPacketError::FbPayloadTypeNotFound: return "PacketFlatbufferPayloadTypeNotFound";
+
+        case EPacketError::Unknown:
+        default: return "Unknown";
+        }
+    }
+} // namespace highp::err
