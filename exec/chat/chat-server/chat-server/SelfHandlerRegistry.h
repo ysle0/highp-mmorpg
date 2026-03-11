020284c62b3dfcf990034120237a9d53dd6b9cd8
Yoonsang Lee <ysle@pm.me>
2026-03-12 01:48:13 +0900

    feat(chat): SelfHandlerRegistry 핸들러 자동 등록 시스템
    REGISTER_HANDLER 매크로를 통해 각 핸들러가 자기 자신을 PacketDispatcher에
    등록하는 self-registration 패턴 도입.
    
    # Conflicts:
    #	exec/chat/chat-server/chat-server/chat-server.vcxproj

---
+++ exec/chat/chat-server/chat-server/SelfHandlerRegistry.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <functional>
+
+#include "server/PacketDispatcher.hpp"
+
+
+/**
+ * IPacketHandler 를 상송하는 각 핸들러 자신이 GameLoop 에 등록하는 데에 사용.
+ * TODO: 이후 크로스 플랫폼으로 진화할 시기에 Dependency Injection, IoC Container 로 대체.
+ */
+class SelfHandlerRegistry {
+public:
+    using FactoryFn = std::function<void(
+        highp::net::PacketDispatcher& dispatcher,
+        std::shared_ptr<highp::log::Logger> logger)>;
+    
+    ~SelfHandlerRegistry() noexcept;
+
+    static SelfHandlerRegistry& Instance();
+    void Add(FactoryFn fn);
+    void RegisterAll(highp::net::PacketDispatcher& dispatcher, std::shared_ptr<highp::log::Logger> logger) const;
+
+private:
+    std::vector<FactoryFn> _factories;
+};
+
+inline SelfHandlerRegistry& SelfHandlerRegistry::Instance() {
+    static SelfHandlerRegistry reg;
+    return reg;
+}
+
+
+/*
+ * @description 핸들러를 등록. 
+ * @param HandlerClass SelfHandlerRegistry::FactoryFn signature
+ */
+#define REGISTER_HANDLER(HandlerClass)          \
+static bool _reg_##HandlerClass = [] {          \
+    SelfHandlerRegistry::Instance().Add([](         \
+        highp::net::PacketDispatcher& d,        \
+        std::shared_ptr<highp::log::Logger> l   \
+    ) {                                         \
+        static HandlerClass h{l};               \
+        d.RegisterHandler(&h);                  \
+    });                                         \
+    return true;                                \
+}();
