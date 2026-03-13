ede63230887612b215877285bed729a2da4e88a1
Yoonsang Lee <ysle@pm.me>
2026-03-14 03:25:02 +0900

    fix: build error

--- exec/chat/chat-server/chat-server/Server.cpp
+++ exec/chat/chat-server/chat-server/Server.cpp
@@ -4,17 +4,14 @@
 #include <scope/Defer.h>
 #include <utility>
 
-using namespace highp;
-
 Server::Server(
-    std::shared_ptr<log::Logger> logger,
-    net::NetworkCfg networkCfg,
-    std::shared_ptr<net::SocketOptionBuilder> socketOptionBuilder
+    std::shared_ptr<highp::log::Logger> logger,
+    highp::net::NetworkCfg networkCfg,
+    std::shared_ptr<highp::net::SocketOptionBuilder> socketOptionBuilder
 ) : _logger(std::move(logger)),
     _socketOptionBuilder(std::move(socketOptionBuilder)),
     _config(networkCfg) {
     _gameLoop = std::make_unique<GameLoop>(_logger, networkCfg);
-    SelfHandlerRegistry::Instance().RegisterAll(_dispatcher, _logger);
 }
 
 Server::~Server() noexcept {
@@ -23,8 +20,8 @@ Server::~Server() noexcept {
     }
 }
 
-Server::Res Server::Start(std::shared_ptr<net::ISocket> listenSocket) {
-    _lifecycle = std::make_unique<net::ServerLifeCycle>(
+Server::Res Server::Start(std::shared_ptr<highp::net::ISocket> listenSocket) {
+    _lifecycle = std::make_unique<highp::net::ServerLifeCycle>(
         _logger,
         _socketOptionBuilder,
         this);
@@ -37,7 +34,7 @@ Server::Res Server::Start(std::shared_ptr<net::ISocket> listenSocket) {
 }
 
 void Server::Stop() {
-    scope::Defer _([this] {
+    highp::scope::Defer _([this] {
         _gameLoop->Stop();
         _hasStopped.store(true);
     });
@@ -49,20 +46,20 @@ void Server::Stop() {
 }
 
 
-void Server::OnAccept(std::shared_ptr<net::Client> client) {
+void Server::OnAccept(std::shared_ptr<highp::net::Client> client) {
     _logger->Debug("[Server::OnAccept]: socket #{}", client->socket);
 }
 
-void Server::OnRecv(std::shared_ptr<net::Client> client, std::span<const char> data) {
-    _logger->Debug("[Server::OnRecv]: socket #{}, data: {}", client->socket, data);
+void Server::OnRecv(std::shared_ptr<highp::net::Client> client, std::span<const char> data) {
+    _logger->Debug("[Server::OnRecv]: socket #{}, data: {}", client->socket, data.data());
     _gameLoop->Receive(client, data);
 }
 
-void Server::OnSend(std::shared_ptr<net::Client> client, size_t bytesTransferred) {
+void Server::OnSend(std::shared_ptr<highp::net::Client> client, size_t bytesTransferred) {
     _logger->Debug("[Server::OnSend]: socket #{}, bytes: {}", client->socket, bytesTransferred);
 }
 
-void Server::OnDisconnect(std::shared_ptr<net::Client> client) {
+void Server::OnDisconnect(std::shared_ptr<highp::net::Client> client) {
     _logger->Debug("[Server::OnDisconnect]: socket #{}", client->socket);
     // TODO: 해당 접속이 끊킨 client 를 room 에서 제거, 세션 정리 등..
 }
---
+++ exec/chat/chat-server/chat-server/handlers/ChatMessageHandler.cpp
@@ -0,0 +1,19 @@
+#include "ChatMessageHandler.h"
+#include "../SelfHandlerRegistry.h"
+#include <utility>
+
+ChatMessageHandler::ChatMessageHandler(std::shared_ptr<highp::log::Logger> logger)
+    : _logger(std::move(logger)) {
+}
+
+void ChatMessageHandler::Handle(
+    std::shared_ptr<highp::net::Client> client,
+    const highp::protocol::messages::ChatMessageBroadcast* payload
+) {
+    _logger->Info("[ChatMessageHandler] socket #{}, room_id={}, message={}",
+                  client->socket, payload->room_id(), payload->message()->c_str());
+
+    // TODO: RoomManager를 통해 해당 방의 모든 유저에게 ChatMessageBroadcast 전송
+}
+
+SELF_REGISTER_PACKET_HANDLER(ChatMessageHandler, true);
