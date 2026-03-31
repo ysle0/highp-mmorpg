9ce7cf91a084586d65a902b1f557a36769dc5aff
Yoonsang Lee <ysle@pm.me>
2026-04-01 01:04:22 +0900

    fix: handler 소유권을 shared_ptr 로 PacketDispatcher 에 이전
    raw new 로 생성된 handler 가 소유자 없이 leak 되던 문제 해결.
    unique_ptr 캡처는 std::move_only_function (C++23) 없이 std::function lambda 에
    캡처 불가하므로, 차선책으로 shared_ptr 을 사용.

--- exec/chat/chat-server/chat-server/SelfHandlerRegistry.h
+++ exec/chat/chat-server/chat-server/SelfHandlerRegistry.h
@@ -13,31 +13,31 @@ class UserManager;
  * IPacketHandler 를 상송하는 각 핸들러 자신이 GameLoop 에 등록하는 데에 사용.
  * TODO: 이후 크로스 플랫폼으로 진화할 시기에 Dependency Injection, IoC Container 로 대체.
  */
-class SelfHandlerRegistry {
+class SelfHandlerRegistry final {
 public:
     using FactoryFn = std::function<void(
-        highp::net::PacketDispatcher& dispatcher,
-        std::shared_ptr<highp::log::Logger> logger,
-        std::shared_ptr<RoomManager> roomManager,
-        std::shared_ptr<UserManager> userManager)>;
+        const std::unique_ptr<highp::net::PacketDispatcher>& dispatcher,
+        const std::shared_ptr<highp::log::Logger>& logger,
+        const std::shared_ptr<RoomManager>& roomManager,
+        const std::shared_ptr<UserManager>& userManager)>;
 
     ~SelfHandlerRegistry() noexcept;
 
     static SelfHandlerRegistry& Instance();
     void Add(FactoryFn fn);
     void RegisterAll(
-        highp::net::PacketDispatcher& dispatcher,
-        std::shared_ptr<highp::log::Logger> logger,
-        std::shared_ptr<RoomManager> roomManager,
-        std::shared_ptr<UserManager> userManager) const;
+        const std::unique_ptr<highp::net::PacketDispatcher>& dispatcher,
+        const std::shared_ptr<highp::log::Logger>& logger,
+        const std::shared_ptr<RoomManager>& roomManager,
+        const std::shared_ptr<UserManager>& userManager) const;
 
 private:
     std::vector<FactoryFn> _factories;
 };
 
 inline SelfHandlerRegistry& SelfHandlerRegistry::Instance() {
-    static SelfHandlerRegistry reg;
-    return reg;
+    static SelfHandlerRegistry r;
+    return r;
 }
 
 
@@ -53,12 +53,16 @@ bool registerSelf(const bool isEnable) {
     if (!isEnable) return false;
 
     SelfHandlerRegistry::Instance().Add(
-        [](highp::net::PacketDispatcher& d,
-           std::shared_ptr<highp::log::Logger> l,
-           std::shared_ptr<RoomManager> roomManager,
-           std::shared_ptr<UserManager> userManager) {
-            d.RegisterHandler<TPayload>(
-                new THandler(std::move(l), std::move(roomManager), std::move(userManager)));
+        [](const std::unique_ptr<highp::net::PacketDispatcher>& dispatcher,
+           const std::shared_ptr<highp::log::Logger>& logger,
+           const std::shared_ptr<RoomManager>& roomManager,
+           const std::shared_ptr<UserManager>& userManager
+    ) {
+            auto handler = std::make_unique<THandler>(
+                logger,
+                roomManager,
+                userManager);
+            dispatcher->RegisterHandler<TPayload>(std::move(handler));
         });
     return true;
 }
